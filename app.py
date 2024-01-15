56f404eb30de95005b3c397411e5ef79cddd25c1
Amit Kumar <138309651+officialamit558@users.noreply.github.com>
2024-01-15 11:31:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def predict():
     return render_template('result.html', prediction=my_prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=int(os.environ.get('PORT', 5000)))
